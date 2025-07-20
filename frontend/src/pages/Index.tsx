48a1e1437d2193a449dd631cc2cfbb21f84102be
Aquib Iqbal <aquibiqbal65@gmail.com>
2025-07-21 00:19:06 +0530

    add fixed social icons

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,15 +1,22 @@
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
-import StatsSection from '../components/StatsSection';
-import WhoWeAreSection from '../components/WhoWeAreSection';
-import HowItWorksSection from '../components/HowItWorksSection';
-import TestimonialsSection from '../components/TestimonialsSection';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import StatsSection from "../components/StatsSection";
+import WhoWeAreSection from "../components/WhoWeAreSection";
+import HowItWorksSection from "../components/HowItWorksSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import Footer from "../components/Footer";
+import SocialIcons from "@/components/ui/socialicons";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
+      
+      {/* Fixed Social Icons - stays in place while scrolling */}
+      <div className="fixed left-0 top-1/2 transform -translate-y-1/2 z-40 hidden lg:block">
+        <SocialIcons />
+      </div>
+      
       <HeroSection />
       <StatsSection />
       <WhoWeAreSection />
